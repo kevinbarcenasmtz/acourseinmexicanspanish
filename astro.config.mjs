075216c0736e9f63f625f1c0dc5b91711a7dcf00
Kevin Barcenas <kevinbarcenas2022@gmail.com>
2025-12-24 03:18:06 -0600

    had to migrate from deprecated tailwind to v4 tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      // Disable the default base styles if you want full control
-      applyBaseStyles: false,
-    }),
-  ],
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
