413d69a1cd6897d31f49795c6f9be58538c003df
Kevin Barcenas <kevinbarcenas2022@gmail.com>
2025-12-24 01:43:04 -0600

    add prettier, eslint configuration

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import eslintPluginAstro from 'eslint-plugin-astro';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    ignores: ['.astro/**', 'dist/**', 'node_modules/**', '.vscode/**'],
+  },
+  ...eslintPluginAstro.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+    },
+  },
+];
